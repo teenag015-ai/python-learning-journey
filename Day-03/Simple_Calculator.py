56c5041d6fd9408cc82cfcb11fdaf263cac328de
teenag015-ai <teenag015@gmail.com>
2026-01-03 12:41:37 +0530

    Add Simple Calculator for basic arithmetic operations

---
+++ Day-03/Simple_Calculator.py
@@ -0,0 +1,8 @@
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+addition=a+b
+print("addition of 2 numbers:",addition)
+subtraction=a-b
+print("subtraction of 2 numbers:",subtraction)
+multiplication=a*b
+print("multiplication of 2 numbers:",multiplication)
