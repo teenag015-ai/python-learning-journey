993ee8d18d05984a2ac432ec493eb5d35bb43863
teenag015-ai <teenag015@gmail.com>
2026-01-03 12:41:02 +0530

    Implement basic login functionality

---
+++ Day-03/Login.py
@@ -0,0 +1,6 @@
+username=input("Enter the username:")
+password=(input("Enter your password")) 
+if username== "admin" and password=="1234":
+    print("Login Successfull")
+else:
+    print("Invalid Credentials")
