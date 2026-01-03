0b08c4a7dc3263926ddfe44d4d02d609491dbf05
teenag015-ai <teenag015@gmail.com>
2026-01-03 12:44:17 +0530

    Add grade calculation based on marks input

---
+++ Day-03/grade.py
@@ -0,0 +1,9 @@
+marks=int(input("Enter the marks(0-100)"))
+if marks>=90:
+    print("Your grade is A")
+elif marks>=75:
+    print("Your grade is B")
+elif marks>=50:
+    print("Your grade is C")
+else:
+    print("you ar fail")
